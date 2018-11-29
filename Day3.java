5bf3761de1e3b37429361b51e5613bb1df51dd09
RohanC <Rohan.chappidi@gmail.com>
2018-11-29 09:50:58 -0500

    Update Day3.java

--- Day3.java
+++ Day3.java
@@ -1,7 +1,7 @@
 public class Day3{
 	public static void main (String[]args){
 		System.out.println("Arjun Patel.");
-		System.out.println("Rohan");
+		System.out.println("Rohan Chappidi.");
 		System.out.println("Riya Sen.");
 		System.out.println("9:44");
 	}
